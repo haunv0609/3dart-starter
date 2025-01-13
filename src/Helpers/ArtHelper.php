0652a0a7a3dde2ec7543b1a8eb090118260691de
Nguyễn Văn Hậu <tiendat141296@gmail.com>
2025-01-13 10:57:01 +0700

    Initial commit

---
+++ src/Helpers/ArtHelper.php
@@ -0,0 +1,8 @@
+<?php
+
+function image(string $path, int $width, int $height): string {
+
+    $data = app(\App\Service\ImagePathGenerator::class)->generate($path, $width, $height);
+
+    return $data;
+}
--- src/StarterServiceProvider.php
+++ src/StarterServiceProvider.php
@@ -3,31 +3,44 @@ namespace haunv\artStarter;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Artisan;
 use Spatie\Permission\PermissionServiceProvider;
 use Spatie\Permission\Middleware\RoleMiddleware;
 use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
-
+use Mckenziearts\Notify\LaravelNotifyServiceProvider;
+use eloquentFilter\ServiceProvider as FilterService;
+use eloquentFilter\Facade\EloquentFilter;
 
 class StarterServiceProvider extends ServiceProvider {
     public function boot()
     {
+        $this->laravelPermission();
+
         $this->app->register(PermissionServiceProvider::class);
+        $this->app->register(LaravelNotifyServiceProvider::class);
+        $this->app->register(FilterService::class);
 
         $this->registerMiddleware();
 
-        $this->publishes([
-            __DIR__.'/config/starter.php' => config_path('starter.php'),
-        ], 'starter-config');
+        $this->publishing();
 
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         $this->loadRoutesFrom(__DIR__.'/routes/starter.php');
+
+        $this->harumcpi();
+        $this->laravelNotify();
+        $this->filemanager();
+        $this->configBreadcrumbs();
     }
     public function register()
     {
         $this->mergeConfigFrom(
             __DIR__.'/config/starter.php', 'starter'
         );
+
+        require_once __DIR__ . '/Helpers/ArtHelper.php';
+
     }
 
     protected function registerMiddleware()
@@ -36,5 +49,78 @@ class StarterServiceProvider extends ServiceProvider {
         $router->aliasMiddleware('permission', \haunv\artStarter\Http\Middleware\PermissionMiddleware::class);
         $router->aliasMiddleware('role', RoleMiddleware::class);
         $router->aliasMiddleware('role_or_permission', RoleOrPermissionMiddleware::class);
+        $router->aliasMiddleware('EloquentFilter', EloquentFilter::class);
+    }
+
+    public function publishing(): void
+    {
+        $this->publishes([
+            __DIR__.'/config/starter.php' => config_path('starter.php'),
+        ], 'starter-config');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations/add_description_to_permissions_and_role_tables.php.stub' => $this->getMigrationFileName('add_description_to_permissions_and_role_tables.php'),
+        ], 'permission-description');
+
+        $this->publishes([
+            __DIR__.'/routes/breadcrumbs.php' => base_path('routes/breadcrumbs.php'),
+        ], 'laravel-breadcrumbs');
+    }
+
+        /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     */
+    protected function getMigrationFileName(string $migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make([$this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR])
+            ->flatMap(fn ($path) => $filesystem->glob($path.'*_'.$migrationFileName))
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
+    }
+
+    public function harumcpi()
+    {
+        Artisan::call('php artisan user-activity:install');
+    }
+
+    public function laravelPermission()
+    {
+        Artisan::call('vendor:publish', [
+            '--provider' => 'Spatie\Permission\PermissionServiceProvider',
+            '--force' => true
+        ]);
+    }
+
+    public function laravelNotify()
+    {
+        Artisan::call('vendor:publish', [
+            '--provider' => 'Mckenziearts\Notify\LaravelNotifyServiceProvider',
+            '--force' => true
+        ]);
+    }
+
+    public function filemanager()
+    {
+        Artisan::call('vendor:publish', [
+            '--tag' => 'lfm_config',
+            '--force' => true
+        ]);
+
+        Artisan::call('vendor:publish', [
+            '--tag' => 'lfm_public',
+            '--force' => true
+        ]);
+    }
+
+    public function configBreadcrumbs()
+    {
+        Artisan::call('vendor:publish', [
+            '--tag' => 'breadcrumbs-config',
+            '--force' => true
+        ]);
     }
 }
